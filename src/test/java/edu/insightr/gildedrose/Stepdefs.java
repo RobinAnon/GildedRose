97aced69966bcd72f6a2d9d5e0bd1916d44c3b31
CAILLON Romain <romain.caillon@devinci.fr>
2018-11-04 03:50:37 +0100

    put conjured.feature in package edu.insightr.gildedrose

--- src/test/java/edu/insightr/gildedrose/Stepdefs.java
+++ src/test/java/edu/insightr/gildedrose/Stepdefs.java
@@ -1,7 +1,5 @@
 package edu.insightr.gildedrose;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
