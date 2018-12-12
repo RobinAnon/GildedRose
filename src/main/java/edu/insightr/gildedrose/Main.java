aae69b73c1434cb1ac0e1ca4c849a47b7953d47e
RobinAnon <43547359+RobinAnon@users.noreply.github.com>
2018-12-12 16:42:12 +0100

    Merge branch 'master' of C:\Users\conve\Desktop\GildedRose with conflicts.

--- src/main/java/edu/insightr/gildedrose/Main.java
+++ src/main/java/edu/insightr/gildedrose/Main.java
@@ -2,12 +2,10 @@ package edu.insightr.gildedrose;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -17,14 +15,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
-import java.util.Hashtable;
 
 public class Main extends Application {
 
     Inventory inventaire = new Inventory();
     Stage window;
     TableView<Item> table;
-    PieChart chart = new PieChart();
 
 
     @Override
@@ -59,46 +55,15 @@ public class Main extends Application {
 
         VBox vbox = new VBox();
         vbox.getChildren().addAll(table, hbox);
-        vbox.getChildren().addAll(chart);
-        updatePieChart();
 
         Scene scene = new Scene(vbox);
         window.setScene(scene);
-
         window.show();
     }
 
     public void updateButtonClicked(){
         inventaire.updateQuality();
-        updatePieChart();
-        table.refresh();
-    }
-
-    public void updatePieChart()
-    {
-        Hashtable<String, Integer> quantities = new Hashtable<>();
-        String[] tab = { "+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros",
-                "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake" };
-        for (String type : tab )
-        {
-            int quantity = 0;
-            for (Item item : inventaire.getItems())
-            {
-                if (item.getName().equals(type))
-                    quantity++;
-            }
-            quantities.put(type, quantity);
-        }
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("Vest", quantities.get("+5 Dexterity Vest")),
-                        new PieChart.Data("Aged Brie", quantities.get("Aged Brie")),
-                        new PieChart.Data("Elixir", quantities.get("Elixir of the Mongoose")),
-                        new PieChart.Data("Sulfuras", quantities.get("Sulfuras, Hand of Ragnaros")),
-                        new PieChart.Data("Backstage passes", quantities.get("Backstage passes to a TAFKAL80ETC concert")),
-                        new PieChart.Data("Mana Cake", quantities.get("Conjured Mana Cake")));
-        chart.setData(pieChartData);
-
+        table.refresh() ;
     }
 
 
